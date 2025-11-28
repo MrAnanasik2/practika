c5656bfbfa8f2a6f1b1bea70c1a9aee37f7f5900
Башкиров Роман Евгеньевич <10230899@college.local>
2025-11-28 17:43:53 +0300

    knopki

--- PRAKTIKA1/MainWindow.xaml.cs
+++ PRAKTIKA1/MainWindow.xaml.cs
@@ -8,17 +8,55 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace PRAKTIKA1
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private List<string> cities = new List<string>() { "Москва", "Санкт-Петербург", "Сочи" };
+
         public MainWindow()
         {
             InitializeComponent();
+
+            Откуда.ItemsSource = cities;
+            Пассажир.ItemsSource = Enumerable.Range(1, 5).ToList();
+            Качество.ItemsSource = new List<string>() { "Эконом", "Комфорт", "Бизнес", "Первый класс" };
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Откуда_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (Откуда.SelectedItem != null)
+            {
+                Куда.ItemsSource = cities.Where(c => c != Откуда.SelectedItem.ToString()).ToList();
+            }
+        }
+
+        private void Куда_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void Куда_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void Пассажир_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void Качество_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
         }
     }
 }
